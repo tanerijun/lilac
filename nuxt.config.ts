5b60d26f29f19c1ad17323f7aa6a46da03f91b7b
tanerijun <tanerivince@gmail.com>
2023-05-12 16:45:16 +0800

    Configure ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ["@/assets/css/global.css"],
-	modules: ["@nuxtjs/tailwindcss"],
-	devtools: {
-		enabled: true,
-	},
+  css: ["@/assets/css/global.css"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/eslint-module"],
+  devtools: {
+    enabled: true,
+  },
 })
