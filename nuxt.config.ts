908aa36a5b0fc91f3d071b0eaf80b0bafff697bd
tanerijun <tanerivince@gmail.com>
2023-05-14 21:22:55 +0800

    Migrate to NextJS

--- nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  css: ["@/assets/css/global.css"],
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/eslint-module"],
-  devtools: {
-    enabled: true,
-  },
-})
---
+++ src/app/(player)/layout.tsx
@@ -0,0 +1,12 @@
+export default function PlayerLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      <h1>Player layout</h1>
+      {children}
+    </div>
+  )
+}
