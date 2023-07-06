25dce403a2631ae99ded8a3e6acff767caa1a599
tanerijun <tanerivince@gmail.com>
2023-07-06 23:59:48 +0800

    More skeleton

--- app/routes/_main._index.tsx
+++ app/routes/_main._index.tsx
@@ -1,3 +1,13 @@
 export default function Main() {
-  return <h1>Hello</h1>
+  return (
+    <main>
+      <section>
+        <h2>Recently played</h2>
+        <div className="flex gap-4">
+          <div>Album 1</div>
+          <div>Liked songs</div>
+        </div>
+      </section>
+    </main>
+  )
 }
